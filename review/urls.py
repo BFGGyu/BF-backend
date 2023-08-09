54b09ed5e0e81dc4dd5118557696f79d3c00b535
Hyeonseo Jeong <jhyeonseo0523@gmail.com>
2023-08-09 19:51:35 +0900

    리뷰 POST 관련 기능 수정

--- review/urls.py
+++ review/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from review.views import *
 
 urlpatterns = [
-    path('', ReviewList.as_view()),
+    path('<str:departure>/<str:arrival>/', ReviewList.as_view()),
     path('<str:departure>/<str:arrival>/<str:writer>/', ReviewDetail.as_view()),
 ]
--- review/views.py
+++ review/views.py
@@ -9,19 +9,21 @@ from django.http import Http404
 
 # Create your views here.
 class ReviewList(APIView):
-    def get(self, request):
-        reviews = Review.objects.all()
-        serializer = ReviewSerializer(reviews, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        departure = request.data.get('departure')
-        arrival = request.data.get('arrival')
-        
+    def get(self, request, departure, arrival):
         try:
             path = Path.objects.get(departure=departure, arrival=arrival)
         except Path.DoesNotExist:
-            return Response({"error": "경로를 찾을 수 없습니다!"}, status=status.HTTP_400_BAD_REQUEST)
+            raise Http404
+
+        reviews = Review.objects.filter(path_id=path.id)
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+
+    def post(self, request,departure, arrival):
+        try:
+            path = Path.objects.get(departure=departure, arrival=arrival)
+        except Path.DoesNotExist:
+            raise Http404
         
         request.data['path_id'] = path.id        
         serializer = ReviewSerializer(data=request.data)
@@ -30,9 +32,9 @@ class ReviewList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class ReviewDetail(APIView):
-    def get(self, request, departure, arrival, writer):
+        
+class ReviewDetail(APIView):             
+    def get(self, request, departure, arrival, writer):          
         try:
             path = Path.objects.get(departure=departure, arrival=arrival)
         except Path.DoesNotExist:
@@ -41,21 +43,39 @@ class ReviewDetail(APIView):
         try:
             review = Review.objects.get(path_id=path.id, writer=writer)
         except Review.DoesNotExist:
-            raise Http404
-            
+            raise Http404        
+
         serializer = ReviewSerializer(review)
         return Response(serializer.data)
 
     def put(self, request, departure, arrival, writer):
-        review = get_object_or_404(Review, writer=writer)
-        serializer = ReviewSerializer(review, data=request.data)
+        try:
+            path = Path.objects.get(departure=departure, arrival=arrival)
+        except Path.DoesNotExist:
+            raise Http404
+
+        try:
+            review = Review.objects.get(path_id=path.id, writer=writer)
+        except Review.DoesNotExist:
+            raise Http404    
+        
+        serializer = ReviewSerializer(review, data=request.data, partial=True)  # 부분 업데이트 가능
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, stauts=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, departure, arrival, writer):
-        review = get_object_or_404(Review, writer=writer)
+        try:
+            path = Path.objects.get(departure=departure, arrival=arrival)
+        except Path.DoesNotExist:
+            raise Http404
+
+        try:
+            review = Review.objects.get(path_id=path.id, writer=writer)
+        except Review.DoesNotExist:
+            raise Http404    
+
         review.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
