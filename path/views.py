b218b46490857c1978bc81ee0405ea9848793dd6
Hyeonseo Jeong <jhyeonseo0523@gmail.com>
2023-08-09 12:31:58 +0900

    경로 좌표 출력 기능 수정: departure, arrival 값 추가 출력

--- path/views.py
+++ path/views.py
@@ -9,14 +9,40 @@ from rest_framework import status
 from django.http import Http404
 
 # Create your views here.
-
 # 경로 좌표 출력
 class RouteList(APIView):
     def get(self, request, departure, arrival):
-        path_id = Path.objects.get(departure=departure, arrival=arrival)
-        routes = Route.objects.filter(path_id=path_id)
+        try:
+            path = Path.objects.get(departure=departure, arrival=arrival)
+        except Path.DoesNotExist:
+            raise Http404
+        
+        routes = Route.objects.filter(path_id=path.id)
         serializer = RouteSerializer(routes, many=True)
-        return Response(serializer.data)
+        
+        departure_latitude = path.departure.latitude
+        departure_longitude = path.departure.longitude
+        arrival_latitude = path.arrival.latitude
+        arrival_longitude = path.arrival.longitude
+
+        departure_info = {
+            "latitude": departure_latitude,
+            "longitude": departure_longitude
+        }
+        
+        arrival_info = {
+            "latitude": arrival_latitude,
+            "longitude": arrival_longitude
+        }
+        
+        response_data = {
+            "path_id": path.id,
+            "departure": departure_info,
+            "arrival": arrival_info,
+            "routes": serializer.data
+        }
+        
+        return Response(response_data)   
     
 # 경로상 장애물 정보 출력
 class ObstacleList(APIView):
