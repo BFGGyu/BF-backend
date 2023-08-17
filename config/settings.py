29da9f602542eac1ee5497dba6254124667717f8
bykbyk0401 <bykbyk0401@gmail.com>
2023-08-17 21:51:15 +0900

    https django 오류

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = ['https://wheelpass.shop']
+
+CORS_REPLACE_HTTPS_REFERER = True
+
 # Application definition
 
 DJANGO_APPS = [
