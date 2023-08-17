a804a8dc148816bcac103b2ab6623d46e51bdabc
Hyeonseo Jeong <jhyeonseo0523@gmail.com>
2023-08-17 20:48:16 +0900

    Review TypeError 해결: reveiw/serializers.py 수정

--- review/serializers.py
+++ review/serializers.py
@@ -11,7 +11,11 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ['id', 'departure', 'arrival', 'writer', 'rating', 'comment', 'created_at', 'updated_at', 'path_id']
 
     def get_departure(self, obj):
-        return obj.path_id.departure if obj.path_id else None
+        if obj.path_id and isinstance(obj.path_id, Path):
+            return str(obj.path_id.departure)  # Path 객체를 문자열로 변환
+        return None
 
     def get_arrival(self, obj):
-        return obj.path_id.arrival if obj.path_id else None
+        if obj.path_id and isinstance(obj.path_id, Path):
+            return str(obj.path_id.arrival)  # Path 객체를 문자열로 변환
+        return None
