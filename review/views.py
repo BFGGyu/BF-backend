f3e664f49d7d0d6823586bce951b241f7bd9a21c
bykbyk0401 <bykbyk0401@gmail.com>
2023-08-18 13:47:09 +0900

    request print

--- review/views.py
+++ review/views.py
@@ -22,11 +22,13 @@ class ReviewList(APIView):
         return Response(serializer.data)
 
     def post(self, request, arrival):
+        print(request)
         try:
             arrival_facility = Facility.objects.get(name=arrival)
             path = Path.objects.get(arrival=arrival_facility)
         except Path.DoesNotExist:
             raise Http404
+        print(request)
         
         request.data['path_id'] = path.id        
         serializer = ReviewSerializer(data=request.data)
